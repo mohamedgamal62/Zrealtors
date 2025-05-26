20418f1c46312230346aae78b4e80608e013c14e
شهاب جمال الدين صالح محمد صالح 1600698 <1600698@eng.asu.edu.eg>
2025-05-26 06:02:42 +0300

    lang

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,7 @@
-import { Language } from './shared/language.enum';
+import { Language } from './shared/translation/language.enum';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { TranslateService } from '@ngx-translate/core';
-
 
 @Component({
   selector: 'app-root',
